869981ff74ccad30b3c075ab5529ba8c1b990a2f
kuro-vale <jsalcedo218@gmail.com>
2022-01-25 16:34:08 -0500

    Add generate random password

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # App
 from app import create_app
+# Python
+import random
+import string
 # Flask
-from flask import request, session, make_response, redirect, render_template
+from flask import request, session, make_response, redirect, render_template, flash, url_for
 from flask_login import login_required, current_user
 
 app = create_app()
@@ -24,6 +27,18 @@ def home():
         "username": username
     }
     return render_template("home.html", **context)
+
+
+@app.route("/random")
+def generate_random_password():
+    characters = list(string.ascii_letters + string.digits + string.punctuation)
+    random.shuffle(characters)
+    random_password = []
+    for i in range(15):
+        random_password.append(random.choice(characters))
+    random.shuffle(random_password)
+    flash("".join(random_password))
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
